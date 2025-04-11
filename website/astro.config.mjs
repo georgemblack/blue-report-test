696cd4c29e3ccfe96815f5497493d034117bd533
George Black <contact@george.black>
2025-04-11 18:53:36 -0500

    Server rendering

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   site: "https://theblue.report",
   integrations: [sitemap()],
   adapter: cloudflare(),
